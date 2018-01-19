e53dd89b7a38a97e0b57a4fefa62706726e90c74
aiji <aiji@SH-L05-cmdb-1-1.(none)>
2018-01-19 14:09:11 +0800

    a

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ export LANGUAGE=UTF-8
 yum groupinstall -y "Development tools"
 yum install -y nano nginx sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 
-wget http://on0aob5yd.bkt.clouddn.com/Python-3.6.0.tar.xz && tar xvJf Python-3.6.0.tar.xz && cd Python-3.6.0
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
 
 ./configure
 make&& make install
