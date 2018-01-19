739742b2759a33b41d21c25f658ab85dd82679b9
AIJIJI <dand@mail.ustc.edu.cn>
2018-01-19 00:29:11 -0500

    Code was clean until manager requested to fuck it up

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@
 
 echo "Python3.6 for Centos 7.0 一键安装脚本\nGitHub主页地址: https://github.com/LunacyZeus\n这个脚本是我在经过多次测试后做出来的,安装了Python要用的所有的必需库(如有缺少,请指出,谢谢!),不会出现在使用中需要某些库而重新编译安装Python的蛋疼事情\n\nPs: 本脚本在腾讯云亲测可用\n  遇到问题请在Github提交BUG"
 
-read -p "请按任意键以继续" var
-
 
 echo "Let's Go..."
 
@@ -15,8 +13,6 @@ export LANGUAGE=zh_CN.UTF-8
 
 echo "安装开发环境工具..."
 yum groupinstall -y "Development tools"
-
-echo "安装编译Python必需工具..."
 yum install -y nano nginx sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-develtk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 
 echo "获取并解压Python3.6源码..."
