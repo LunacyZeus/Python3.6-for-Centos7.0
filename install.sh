cc29d36afa0a754240854ba184600f83a17d518e
LunacyZeus <LunacyZeus@users.noreply.github.com>
2018-05-05 18:50:22 +0800

    修改了脚本
    python更新到了python3.65
    鉴于阿里云访问python官方源太慢 这里为国内做了七牛云的镜像加速

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #coding:utf-8
-
-
 export LANG=UTF-8
 export LANGUAGE=UTF-8
+
+UPPDATE_TEXT_COLOR='\E[1;31m'
+RES='\E[0m'
+echo -e  "${UPPDATE_TEXT_COLOR}======2018/05/05更新======\n\n1.Python版本更新到3.6.5\n2.鉴于阿里云访问python官方源太慢,这里做了七牛cdn的加速\n\n\n${RES}"
 
 yum groupinstall -y "Development tools"
 yum install -y nano nginx sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 
-wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
+wget -O python.tar.xz http://p894dckis.bkt.clouddn.com/Python-3.6.5.tar.xz && tar xvJf python.tar.xz && cd Python-3.6.4
 
 ./configure
 make&& make install
